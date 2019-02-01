0b9b2439f4bc9a416c02cf1662bfd20fbf0d0308
Cristian-PC\Cristian <cristiangaetejordan@gmail.com>
2019-02-01 03:25:28 -0300

    Primer paso Reporte 1.96

---
+++ Reportes/Reportes/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Reportes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabReportes = new System.Windows.Forms.TabControl();
+            this.tabR196 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnGuardar196 = new System.Windows.Forms.Button();
+            this.ofdAbrirDocumento = new System.Windows.Forms.OpenFileDialog();
+            this.panel1.SuspendLayout();
+            this.tabReportes.SuspendLayout();
+            this.tabR196.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnBuscar);
+            this.panel1.Location = new System.Drawing.Point(-2, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(969, 78);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(14, 12);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(113, 35);
+            this.btnBuscar.TabIndex = 0;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(133, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Archivo";
+            // 
+            // tabReportes
+            // 
+            this.tabReportes.Controls.Add(this.tabR196);
+            this.tabReportes.Controls.Add(this.tabPage2);
+            this.tabReportes.Location = new System.Drawing.Point(-2, 84);
+            this.tabReportes.Name = "tabReportes";
+            this.tabReportes.SelectedIndex = 0;
+            this.tabReportes.Size = new System.Drawing.Size(969, 457);
+            this.tabReportes.TabIndex = 1;
+            // 
+            // tabR196
+            // 
+            this.tabR196.Controls.Add(this.btnGuardar196);
+            this.tabR196.Controls.Add(this.dataGridView1);
+            this.tabR196.Location = new System.Drawing.Point(4, 22);
+            this.tabR196.Name = "tabR196";
+            this.tabR196.Padding = new System.Windows.Forms.Padding(3);
+            this.tabR196.Size = new System.Drawing.Size(961, 431);
+            this.tabR196.TabIndex = 0;
+            this.tabR196.Text = "Reporte 1.96";
+            this.tabR196.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(961, 431);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(707, 435);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnGuardar196
+            // 
+            this.btnGuardar196.Location = new System.Drawing.Point(714, 376);
+            this.btnGuardar196.Name = "btnGuardar196";
+            this.btnGuardar196.Size = new System.Drawing.Size(123, 44);
+            this.btnGuardar196.TabIndex = 1;
+            this.btnGuardar196.Text = "Guardar 1.96";
+            this.btnGuardar196.UseVisualStyleBackColor = true;
+            // 
+            // ofdAbrirDocumento
+            // 
+            this.ofdAbrirDocumento.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(964, 538);
+            this.Controls.Add(this.tabReportes);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tabReportes.ResumeLayout(false);
+            this.tabR196.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TabControl tabReportes;
+        private System.Windows.Forms.TabPage tabR196;
+        private System.Windows.Forms.Button btnGuardar196;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.OpenFileDialog ofdAbrirDocumento;
+    }
+}
+
---
+++ Reportes/Reportes/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reportes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ofdAbrirDocumento.Filter = "Archivos csv (*.csv)|*.csv|Documentos Excel 2003 (*.xls)|*.xls|Documentos Excel 2007-2010 (*.xlsx)|*.xlsx";
+                DataTable DtReporte196 = new DataTable();
+
+               
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            
+
+        }
+
+        public DataTable(String ruta196)   
+        {
+            try
+            {
+                String LineaTexto = String.Empty;
+                String nombre_archivo196;
+                DataTable dtReporte196 = new DataTable("Reporte196");
+
+                dtReporte196.Columns.Add("Unidad de Negocio", typeof(String));
+                dtReporte196.Columns.Add("Fecha", typeof(DateTime));
+                dtReporte196.Columns.Add("PPU", typeof(String));
+                dtReporte196.Columns.Add("Tipo de GPS", typeof(String));
+                dtReporte196.Columns.Add("Servicio", typeof(String));
+                dtReporte196.Columns.Add("Sentido", typeof(String));
+                dtReporte196.Columns.Add("Intervalo", typeof(String));
+                dtReporte196.Columns.Add("Operativo",typeof(String));
+                dtReporte196.Columns.Add("KM Intervalo", typeof(Double));
+                dtReporte196.Columns.Add("KM del viaje en intervalo", typeof(Double));
+                dtReporte196.Columns.Add("Fecha Inicio Viaje", typeof(DateTime));
+                dtReporte196.Columns.Add("Fecha Fin Viaje",typeof(DateTime));
+                dtReporte196.Columns.Add("Tiempo de este viaje", typeof(Double));
+                dtReporte196.Columns.Add("Tiempo total de viaje del intervalo", typeof(Double));
+                dtReporte196.Columns.Add("Tiempo de Viaje", typeof(String));
+                dtReporte196.Columns.Add("Inicio y Fin de Ruta", typeof(String));
+                dtReporte196.Columns.Add("120 Minutos Antes", typeof(String));
+                dtReporte196.Columns.Add("120 Minutos Despues", typeof(String));
+                dtReporte196.Columns.Add("PC cada 2 Km", typeof(String));
+                dtReporte196.Columns.Add("Igual Servicio", typeof(String));
+
+                String[] SplitLine; ;
+                Int32 intContador=0;
+                nombre_archivo196 = System.IO.Path.GetFileName(ruta196);
+
+                label1.Text = nombre_archivo196;
+
+                if (System.IO.File.Exists(ruta196) == true)
+                {
+                    Int32 contColumnas = 0;
+                    var objReader = new System.IO.StreamReader(ruta196);
+                    while (objReader.Peek() != -1)
+                    {
+                        LineaTexto = objReader.ReadLine();
+                        SplitLine = LineaTexto.Split(';');
+                        contColumnas = SplitLine.Count();
+
+                        if (intContador > 7)
+                        {
+                            Dictionary<String, String> diccionario = new Dictionary<string, string>();
+                            diccionario.Add("I", "Ida");
+                            diccionario.Add("IDA", "Ida");
+                            diccionario.Add("R", "Ret");
+                            diccionario.Add("RETORNO", "Ret");
+                            diccionario.Add("REGRESO", "Ret");
+
+                            String unidad_negocio = SplitLine[0].Trim();
+                            String PPU = SplitLine[2].Trim();
+                            String GPS = SplitLine[3].Trim();
+                            String Servicio = SplitLine[4].Replace(" ","").ToUpper();
+                            String Sentido = SplitLine[5].Trim().ToUpper();
+                            String Intervalo = SplitLine[6].Trim();
+                            String Operativo = SplitLine[7].Trim();
+                        }
+                    }
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            
+
+
+        }
+    }
+}
