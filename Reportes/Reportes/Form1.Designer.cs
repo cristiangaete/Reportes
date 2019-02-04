bf231be502467c17a6fb126ead29bdfbc58c5bab
Cristian-PC\Cristian <cristiangaetejordan@gmail.com>
2019-02-04 01:56:52 -0300

    Agregamos las columnas en el reporte 2.2

--- Reportes/Reportes/Form1.Designer.cs
+++ Reportes/Reportes/Form1.Designer.cs
@@ -29,18 +29,18 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.btnBuscar = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.btnBuscar = new System.Windows.Forms.Button();
             this.tabReportes = new System.Windows.Forms.TabControl();
             this.tabR196 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.btnGuardar196 = new System.Windows.Forms.Button();
+            this.dgvReporte196 = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.ofdAbrirDocumento = new System.Windows.Forms.OpenFileDialog();
             this.panel1.SuspendLayout();
             this.tabReportes.SuspendLayout();
             this.tabR196.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReporte196)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -52,6 +52,15 @@
             this.panel1.Size = new System.Drawing.Size(969, 78);
             this.panel1.TabIndex = 0;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(133, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Archivo";
+            // 
             // btnBuscar
             // 
             this.btnBuscar.Location = new System.Drawing.Point(14, 12);
@@ -61,15 +70,6 @@
             this.btnBuscar.Text = "Buscar";
             this.btnBuscar.UseVisualStyleBackColor = true;
             this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(133, 12);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(43, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Archivo";
             // 
             // tabReportes
             // 
@@ -84,7 +84,7 @@
             // tabR196
             // 
             this.tabR196.Controls.Add(this.btnGuardar196);
-            this.tabR196.Controls.Add(this.dataGridView1);
+            this.tabR196.Controls.Add(this.dgvReporte196);
             this.tabR196.Location = new System.Drawing.Point(4, 22);
             this.tabR196.Name = "tabR196";
             this.tabR196.Padding = new System.Windows.Forms.Padding(3);
@@ -92,24 +92,6 @@
             this.tabR196.TabIndex = 0;
             this.tabR196.Text = "Reporte 1.96";
             this.tabR196.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(961, 431);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(707, 435);
-            this.dataGridView1.TabIndex = 0;
             // 
             // btnGuardar196
             // 
@@ -120,9 +102,23 @@
             this.btnGuardar196.Text = "Guardar 1.96";
             this.btnGuardar196.UseVisualStyleBackColor = true;
             // 
-            // ofdAbrirDocumento
+            // dgvReporte196
             // 
-            this.ofdAbrirDocumento.FileName = "openFileDialog1";
+            this.dgvReporte196.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvReporte196.Location = new System.Drawing.Point(0, 0);
+            this.dgvReporte196.Name = "dgvReporte196";
+            this.dgvReporte196.Size = new System.Drawing.Size(707, 435);
+            this.dgvReporte196.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(961, 431);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -137,7 +133,7 @@
             this.panel1.PerformLayout();
             this.tabReportes.ResumeLayout(false);
             this.tabR196.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReporte196)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -150,7 +146,7 @@
         private System.Windows.Forms.TabControl tabReportes;
         private System.Windows.Forms.TabPage tabR196;
         private System.Windows.Forms.Button btnGuardar196;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvReporte196;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.OpenFileDialog ofdAbrirDocumento;
     }
--- Reportes/Reportes/Form1.cs
+++ Reportes/Reportes/Form1.cs
@@ -16,32 +16,63 @@ namespace Reportes
         {
             InitializeComponent();
         }
+        public static bool IsDate(Object obj)
+        {
+            string strDate = obj.ToString();
+            try
+            {
+                DateTime dt = DateTime.Parse(strDate);
+                if ((dt.Month != System.DateTime.Now.Month) || (dt.Day < 1 && dt.Day > 31) || dt.Year != System.DateTime.Now.Year)
+                    return false;
+                else
+                    return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool IsNumeric(String s)
+        {
+            return s.All(Char.IsDigit);
+        }
+
+
 
         private void btnBuscar_Click(object sender, EventArgs e)
         {
             try
             {
-                ofdAbrirDocumento.Filter = "Archivos csv (*.csv)|*.csv|Documentos Excel 2003 (*.xls)|*.xls|Documentos Excel 2007-2010 (*.xlsx)|*.xlsx";
-                DataTable DtReporte196 = new DataTable();
+                //Aqui se cargan y se leen los archivos cvs 
 
+                ofdAbrirDocumento.Filter = "Archivos csv (*.csv)|*.csv|Documentos Excel 2003 (*.xls)|*.xls|Documentos Excel 2007-2010 (*.xlsx)|*.xlsx";
+
+                if (ofdAbrirDocumento.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    DataTable dtReporte196 = new DataTable();
+                    dtReporte196 = GetDataCSV196(ofdAbrirDocumento.FileName);
+                    dgvReporte196.DataSource = dtReporte196;
+                    tabReportes.SelectedIndex = 0;
+                }
                
-            }
+
+               }
             catch (Exception ex)
             {
 
                 MessageBox.Show(ex.Message);
             }
             
-
         }
 
-        public DataTable(String ruta196)   
+        public DataTable GetDataCSV196(string ruta196)   
         {
             try
             {
                 String LineaTexto = String.Empty;
                 String nombre_archivo196;
-                DataTable dtReporte196 = new DataTable("Reporte196");
+                 DataTable dtReporte196 = new DataTable("Reporte196");
 
                 dtReporte196.Columns.Add("Unidad de Negocio", typeof(String));
                 dtReporte196.Columns.Add("Fecha", typeof(DateTime));
@@ -96,21 +127,92 @@ namespace Reportes
                             String Sentido = SplitLine[5].Trim().ToUpper();
                             String Intervalo = SplitLine[6].Trim();
                             String Operativo = SplitLine[7].Trim();
+                            String tiempos_viaje = SplitLine[14];
+                            String Inicio_fin_ruta = SplitLine[15];
+                            String min_120_antes = SplitLine[16];
+                            String min_120_despues = SplitLine[17];
+                            String pc_cada2km = SplitLine[18];
+                            String igual_servicio = SplitLine[19];
+
+                            ///Creamos las filas en el DataTable
+                           
+                            DataRow NuevaFila = dtReporte196.NewRow();
+
+                            NuevaFila[0] = unidad_negocio;
+
+                            if (IsDate(SplitLine[1]))
+                            {
+                                NuevaFila[1] = Convert.ToDateTime(SplitLine[1]);
+                            }
+
+                            NuevaFila[2] = PPU;
+                            NuevaFila[3] = GPS;
+
+                            NuevaFila[4] = Servicio;
+
+                            NuevaFila[5] = Sentido;
+                            NuevaFila[6] = Intervalo;
+                            NuevaFila[7] = Operativo;
+
+                            if (IsNumeric(SplitLine[8]))
+                            {
+                                NuevaFila[8] = SplitLine[8];
+                            }
+
+                            if (IsNumeric(SplitLine[9]))
+                            {
+                                NuevaFila[9] = SplitLine[9];
+                            }
+
+                            if (IsDate(SplitLine[10]))
+                            {
+                                NuevaFila[10] = Convert.ToDateTime(SplitLine[10]);
+                            }
+
+                            if (IsDate(SplitLine[11]))
+                            {
+                                NuevaFila[11] = Convert.ToDateTime(SplitLine[11]);
+                            }
+
+                            if (IsNumeric(SplitLine[12]))
+                            {
+                                NuevaFila[12] = SplitLine[12];
+                            }
+
+                            if (IsNumeric(SplitLine[13]))
+                            {
+                                NuevaFila[13] = SplitLine[13];
+                            }
+
+                            NuevaFila[14] = tiempos_viaje;
+                            NuevaFila[15] = Inicio_fin_ruta;
+                            NuevaFila[16] = min_120_antes;
+                            NuevaFila[17] = min_120_despues;
+                            NuevaFila[18] = pc_cada2km;
+                            NuevaFila[19] = igual_servicio;
+
+                            dtReporte196.Rows.Add(NuevaFila);
+
                         }
+                        intContador = intContador + 1;
                     }
+                    objReader.Close();
+
                 }
-
-
-
+                else
+                {
+                    MessageBox.Show("No Existe el Archivo");
+                }
+                return dtReporte196;
             }
             catch (Exception ex)
             {
 
                 MessageBox.Show(ex.Message);
+                return null;
             }
+
             
-
-
         }
     }
 }
